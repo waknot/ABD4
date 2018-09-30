b448445bf1960433fd7e8c2fc03d2acd84a5f660
aurelienCastellarnau <castelllarnau.a@gmail.com>
2018-10-01 01:17:51 +0200

    feat(main/App): server launchers: one will wait listening, the other is waiting a channel supposed to be launched in a goroutine
        -  App.Initialize: take <context.IServerOption>*server.Option to instanciate the context
        - Pass the context to the Routing() function
        - launch
    
        - App.InitializeAndWaitForSignal: take <context.IServerOption>*server.Option to instanciate the context and a chan bool
        - Pass the context to the Routing() function
        - put true into the channel, if all threads are sleeping without listening on this channel, we fall in a deadlock
        - launch the server

--- API/main.go
+++ API/main.go
@@ -1,20 +1,51 @@
+/*
+ * File: README.md
+ * Project: ABD4/VMD Escape Game
+ * File Created: Sunday, 30th September 2018 2:24:43 pm
+ * Author: billaud_j castel_a masera_m
+ * Contact: (billaud_j@etna-alternance.net castel_a@etna-alternance.net masera_m@etna-alternance.net)
+ * -----
+ * Last Modified: Sunday, 30th September 2018 5:44:39 pm
+ * Modified By: Aurélien Castellarnau
+ * -----
+ * Copyright © 2018 - 2018 billaud_j castel_a masera_m, ETNA - VDM EscapeGame API
+ */
+
 package main
 
 import (
-	"encoding/json"
+	"ABD4/API/server"
+	"flag"
 	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"os"
+	"path/filepath"
+	"strconv"
 )
 
-func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/", NewReservation).Methods("POST")
+var app *App
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+func launchApp(opts *server.Option) {
+	app = &App{}
+	err := app.Initialize(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func NewReservation(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("reservation ok")
+func main() {
+	var ip = flag.String("ip", "0.0.0.0", "define ip address")
+	var env = flag.String("env", "dev", "define environnement context, [prod|dev|test]")
+	var port = flag.Int("p", 8000, "define port")
+	var debug = flag.Bool("d", false, "active debug logging")
+	var logpath = flag.String("logpath", "", "define log folder path from exe folder")
+	var datapath = flag.String("datapath", "", "define data folder path from exe folder")
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	flag.Parse()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	portStr := strconv.Itoa(*port)
+	opts := &server.Option{}
+	opts.Hydrate(portStr, *ip, *env, dir, *logpath, *datapath, *debug)
+	launchApp(opts)
 }
